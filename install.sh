dfead4cfce6b1c73fa8360ff6594ade638617872
LowderPlay <lowderchannel@gmail.com>
2017-11-25 12:31:12 +0500

    install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ else
 	sudo mkdir /tmp/nyt/
 	cd /tmp/nyt/
 	echo "Installing dependencies..."
-	sudo apt install apache2 stty arduino python3 git
+	sudo apt install apache2 arduino python3 git
 	echo "Installing ino..."
 	sudo git clone git://github.com/amperka/ino.git
 	cd ino/
@@ -14,10 +14,10 @@ else
 	echo "Cloning files..."
 	sudo git clone https://github.com/LowderPlay/NewYearTree
 	echo "Uploading code to Arduino..."
-	cd /tmp/nyt/arduino/WiFi_light_2/
+	cd /tmp/nyt/NewYearTree/arduino/WiFi_light_2/
 	ino build
 	ino upload
 	echo "Copying files to web server..."
-	sudo cp -R /tmp/nyt/web/NewYearTree /var/www/html/tree
+	sudo cp -R /tmp/nyt/NewYearTree/web/ /var/www/html/tree
 	echo "Installed! You can access the web interface in http://RASPBERRY_PI_IP/tree"
 fi
